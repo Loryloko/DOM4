7fa1d582b25508a75039c7383e6b22c6df4c7c7b
Lorenzo <Lorenzomem56@gmail.com>
2026-05-29 18:12:37 +0200

    Dom4

--- EsDOM/4/script.js
+++ EsDOM/4/script.js
@@ -1,53 +1,40 @@
-const inputSecondi = document.getElementById('inputSecondi')
-const btnStart = document.getElementById('CD');
-const btnPausa = document.getElementById('Pausa');
-const btnReset = document.getElementById('Reset');
-const timerDisplay = document.getElementById('timerDisplay');
+let remainingTime = document.querySelector('#remainingTime')
+let countdownInput = document.querySelector('#countdown-input')
 
-let intervalId = null;
-let secondiRimasti = 0;
+let startBtn = document.querySelector('#startBtn')
+let stopBtn = document.querySelector('#stopBtn')
+let resetBtn = document.querySelector('#resetBtn')
 
-function aggiornaDisplay(valore) {
-    timerDisplay.textContent = valore;
-}
+let interval;
+let counter;
+let remainingSeconds = 0
 
-btnStart.addEventListener('click', () => {
-    if(intervalId !== null) return;
+startBtn.addEventListener('click', () =>{
+    clearInterval(interval)
+    counter = countdownInput.value
 
-    if(secondiRimasti <= 0){
-        secondiRimasti = parseInt(inputSecondi.value);
+    if(remainingSeconds !== 0){
+        counter = remainingSeconds
     }
-    if(isNaN(secondiRimasti) || secondiRimasti <= 0){
-        alert("inserisci un numero valido di secondi superiore a 0")
-        return;
-    }
-    aggiornaDisplay(secondiRimasti)
 
-    intervalId = setInterval(() => {
-        secondiRimasti--;
-        aggiornaDisplay(secondiRimasti);
-        
-        if(secondiRimasti <= 0){
-            clearInterval(intervalId)
-            intervalId = null
-            alert('Tempo scaduto')
+    interval = setInterval(() => {
+        if(counter < 0){
+            clearInterval(interval)
+            remainingTime.innerHTML = 'Tempo scaduto'
+        }else{
+            remainingTime.innerHTML = counter
+            counter --
         }
     }, 1000)
 })
 
-btnPausa.addEventListener('click', () => {
-    if(intervalId !== null){
-        clearInterval(intervalId)
-        intervalId = null
-    }
+stopBtn.addEventListener('click', () =>{
+    clearInterval(interval)
+    remainingSeconds = counter
 })
 
-btnReset.addEventListener('click', () => {
-    if(intervalId !== null){
-        clearInterval(intervalId)
-        intervalId = null
-    }
-    secondiRimasti = 0;
-    inputSecondi.value = '';
-    aggiornaDisplay(0);
+resetBtn.addEventListener('click', () =>{
+    countdownInput.value = ''
+    clearInterval(interval)
+    remainingTime.innerHTML = ''
 })
